d0509a397e224e7498a9f828b376e4a3e407c001
MoonLord2436 <97262816+MoonLord2436@users.noreply.github.com>
2023-10-01 12:44:34 -0700

    Upload YelpCamp version 3

---
+++ v3/app.js
@@ -0,0 +1,105 @@
+// Variables
+
+express = require("express")
+app = express()
+bodyParser = require("body-parser")
+Camp = require("./models/Camp");
+comments = require("./models/Comments");
+seedFunc = require("./seeds")
+// mongoose = require("mongoose")
+// mongoose.connect("mongodb://localhost/Campgrounds")
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/Campgrounds", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB!'))
+.catch(error => console.log(error.message));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+app.use(express.static("views"))
+
+
+seedFunc();
+
+// Get Requests
+
+app.get("/", function(req, res){
+	res.render("HomePage")
+})
+
+
+app.get("/campgrounds", function(req, res){
+	CampsiteSearcher = Camp.find({}, function(err, item){
+		if(err){
+			console.log("ERROR!")
+			console.log(err)
+		}
+		else {
+				res.render("Campgrounds", {CampsiteSearcher: item})
+			
+		}
+	})
+})
+
+
+app.get("/PostCampsite", function(req, res){
+	res.render("PostCampsite")
+})
+ 
+
+
+app.get("/campgrounds/:id", function(req, res){
+	Camp.findById(req.params.id).populate("comment").exec(function(err, success){
+		if(err){
+			console.log("ERROR!");
+			console.log(err);
+		}
+		else {
+			res.render("show.ejs", {campground: success})
+		}
+	})
+})
+
+
+// Post Requests
+
+app.post("/campgrounds", function(req, res ){
+	name = req.body.PostCampInput;
+	image = req.body.PostCampPic;
+	des = req.body.PostCampDes;
+	Camp.create({
+		name: name,
+		image: image,
+		description: des 
+	},
+		function(err, item){
+		if(err){
+			console.log("ERROR!");
+			console.log(err)
+		}
+		else {
+			res.redirect("/campgrounds")
+		}
+	})
+})
+
+
+
+// Mongoose
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+	console.log("You've started the server")
+})
